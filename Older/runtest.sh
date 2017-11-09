c1a24dbdddb2acd7002793dd734b660edb654f23
Jared Page <me@jaredpage.net>
2017-11-10 09:20:29 +1100

    Removed older files

--- Older/runtest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TOTP=430694
-docker run --rm isam_freeradius radtest sweeden@au1.ibm.com "$TOTP" 169.44.117.85:22 0 SECRET
--- Older/testclient.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-radtest sweeden@au1.ibm.com "$1" localhost 0 SECRET
